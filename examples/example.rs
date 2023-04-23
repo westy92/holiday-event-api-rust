c874bd706e8baba6aa38026c85bf0e4ea1649e3b
Seth Westphal <westy92@gmail.com>
2023-04-22 20:37:29 -0500

    Add example.

--- examples/example.rs
+++ examples/example.rs
@@ -1,22 +1,3 @@
-# The Official Holiday and Event API for Rust
-
-[![Funding Status](https://img.shields.io/github/sponsors/westy92)](https://github.com/sponsors/westy92)
-
-Industry-leading Holiday and Event API for Rust. Over 5,000 holidays and thousands of descriptions. Trusted by the World’s leading companies. Built by developers for developers since 2011.
-
-## Authentication
-
-Access to the Holiday and Event API requires an API Key. You can get for one for FREE [here](https://apilayer.com/marketplace/checkiday-api#pricing), no credit card required! Note that free plans are limited. To access more data and have more requests, a paid plan is required.
-
-## Installation
-
-```console
-cargo add holiday_event_api
-```
-
-## Example
-
-```rust
 use holiday_event_api::{HolidayEventApi, model::{GetEventsRequest, RateLimited, GetEventInfoRequest, SearchRequest}};
 
 #[tokio::main]
@@ -82,4 +63,3 @@ async fn main() {
     let search = search.unwrap();
     println!("Found {} events, including {}, that match the query \"{}\".", search.events.len(), search.events.first().unwrap().name, query)
 }
-```
